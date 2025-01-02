5dfe699fb993220f9ac8b6235b02e9dbd03bf3cd
Dineshv64 <vd648864@gmail.com>
2025-01-02 09:51:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+//sidebar
+
+var sidenavbar = document.querySelector(".sidebar-container")
+
+function showbar(){
+    sidenavbar.style.left="0%"
+}
+function closebar(){
+    sidenavbar.style.left="-60%"
+}
